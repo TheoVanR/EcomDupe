b3291329367243d589e4437a77fc11ed5faf3938
Theodor van Reisen <98526113+TheoVanR@users.noreply.github.com>
2025-01-14 21:33:29 +0100

    2025

--- app/Products/page.js
+++ app/Products/page.js
@@ -38,6 +38,8 @@ export default function Page({ product }) {
                                 <Link href="./Products/ProductInfo">{product.title}</Link>
                                 <p>{product.Category}</p>
                                 <p>Price: {product.price.toFixed(2)}</p>
+                                <button onClick={() => addToCart(product)} type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Add to cart</button>
+
                             </div>
                         </div>
 
